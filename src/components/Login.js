310b655763996f8cd18891bc0558691baaa369be
EliaRitzmann <69593308+EliaRitzmann@users.noreply.github.com>
2021-12-08 10:39:29 +0100

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 const AUTH_URL =
-"https://accounts.spotify.com/authorize?client_id=1d9716b896714fdb9bbc9f0de3195d42&response_type=code&redirect_uri=https://statify-bbbaden.azurewebsites.net/&show_dialog=true&scope=user-top-read%20user-read-recently-played%20user-read-private%20user-read-email"
+"https://accounts.spotify.com/authorize?client_id=1d9716b896714fdb9bbc9f0de3195d42&response_type=code&redirect_uri=http://localhost:3000/&show_dialog=true&scope=user-top-read%20user-read-recently-played%20user-read-private%20user-read-email"
 
  
  export default function Login(){
